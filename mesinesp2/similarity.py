8502f03c13327cbd6b30ebdeb6b494303b8c3183
redhijosbello <matirojasga@gmail.com>
2021-05-09 22:42:19 -0400

    Top k cosine similarity test

--- mesinesp2/similarity.py
+++ mesinesp2/similarity.py
@@ -39,15 +39,20 @@ def similarity(data, model, idf):
     decs_matrix = np.array([v for v in decs_embs.values()])
     return cosine_similarity(abstract_matrix, decs_matrix)
 
-   
+def top_k_values(array):
+    indexes = array.argsort()[-10:][::-1]
+    A = set(indexes)
+    B = set(list(range(array.shape[0])))
+    array[list(B.difference(A))]=0
+    return array
 
     
 
 if __name__ == '__main__':
     descriptions = get_descriptions('../data/raw/DeCS2020.obo')
     train, dev, test = mesinesp2.data.load_dataset('../data/raw', 1)
-    #x_train, y_train = transform(train, descriptions, split_sentences = False)
-    x_dev, y_dev = transform(dev, descriptions, split_sentences = False)
+    #x_train, y_train = transform(train, descriptions, split_sentences = False, transform_labels = False)
+    x_dev, y_dev = transform(dev, descriptions, split_sentences = False, transform_labels = False)
 
     # We first create each embedding file. It contains the embedding representation for each DecS code.
     idf = create_decs_embeddings()
@@ -57,12 +62,32 @@ if __name__ == '__main__':
     #print(train_sbw_similarity.shape)
     dev_sbw_similarity = similarity(x_dev, 'sbw', idf)
     print(dev_sbw_similarity.shape)
-    
+    print(dev_sbw_similarity[0, :])
+    #print(np.max(dev_sbw_similarity, axis=1))
+
+    result = np.apply_along_axis(top_k_values, 1, dev_sbw_similarity)
+    #print(result.shape)
+    #print(result)
+
+    for i, abs in enumerate(x_dev):
+        print('-------------------------')
+        # Abstract
+        print(abs)
+        top = np.where(result[i,:] != 0)[0]
+        ar = []
+        for val in top:
+            ar.append(list(descriptions.keys())[val])
+        # Predicted codes with the comparison of embeddings
+        print('---------------------- Predicted -----------------')
+        print(ar)
+        print('---------------------- Real ----------------------')
+        print(y_dev[i])
+        print('-------------------------')
     # Cosine similarity with mix
     #train_mix_similarity = similarity(x_train, 'mix', idf)
     #print(train_mix_similarity.shape)
-    dev_mix_similarity = similarity(x_dev, 'mix', idf)
-    print(dev_mix_similarity.shape)
+    #dev_mix_similarity = similarity(x_dev, 'mix', idf)
+    #print(dev_mix_similarity.shape)
     
     
     
